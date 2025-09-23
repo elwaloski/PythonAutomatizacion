21dd4a43332637d46b080f9b4be12627903a11b7
elwaloski <el.waloski@gmail.com>
2025-09-23 11:18:31 -0300

    act
    act

--- config_ambiente/IISCambio_QA_Desa.bat
+++ config_ambiente/IISCambio_QA_Desa.bat
@@ -1,29 +1,29 @@
-set OldHost=fe-qa-admin.cl.dbnetcorp.com
-set NewHost=fe-desa-admin.cl.dbnetcorp.com
+set OldHost=fe-qa-admin.cl.xxxxxx.com
+set NewHost=fe-desa-admin.cl.xxxxx.com
 powershell -Command "& {Import-Module WebAdministration; Rename-Item "IIS:\Sites\%OldHost%" %NewHost%}"&
 powershell -Command "& {Import-Module WebAdministration; Set-WebBinding -Name %NewHost% -BindingInformation '*:80:%OldHost%' -PropertyName HostHeader -Value %NewHost%}"&
 powershell -Command "& {Import-Module WebAdministration; Set-WebBinding -Name %NewHost% -BindingInformation '*:443:%OldHost%' -PropertyName HostHeader -Value %NewHost%}"&
 
-set OldHost=cf-qa.cl.dbnetcorp.com
-set NewHost=cf-desa.cl.dbnetcorp.com
+set OldHost=cf-qa.cl.xxxxx.com
+set NewHost=cf-desa.cl.xxxxx.com
 powershell -Command "& {Import-Module WebAdministration; Rename-Item "IIS:\Sites\%OldHost%" %NewHost%}"&
 powershell -Command "& {Import-Module WebAdministration; Set-WebBinding -Name %NewHost% -BindingInformation '*:80:%OldHost%' -PropertyName HostHeader -Value %NewHost%}"&
 powershell -Command "& {Import-Module WebAdministration; Set-WebBinding -Name %NewHost% -BindingInformation '*:443:%OldHost%' -PropertyName HostHeader -Value %NewHost%}"&
 
-set OldHost=fe-qa.cl.dbnetcorp.com
-set NewHost=fe-desa.cl.dbnetcorp.com
+set OldHost=fe-qa.cl.xxxxxx.com
+set NewHost=fe-desa.cl.xxxxxx.com
 powershell -Command "& {Import-Module WebAdministration; Rename-Item "IIS:\Sites\%OldHost%" %NewHost%}"&
 powershell -Command "& {Import-Module WebAdministration; Set-WebBinding -Name %NewHost% -BindingInformation '*:443:%OldHost%' -PropertyName HostHeader -Value %NewHost%}"&
 
-set OldHost=fe-qa-wss.cl.dbnetcorp.com
-set NewHost=fe-desa-wss.cl.dbnetcorp.com
+set OldHost=fe-qa-wss.cl.xxxxxxx.com
+set NewHost=fe-desa-wss.cl.xxxxxxx.com
 powershell -Command "& {Import-Module WebAdministration; Rename-Item "IIS:\Sites\%OldHost%" %NewHost%}"&
 powershell -Command "& {Import-Module WebAdministration; Set-WebBinding -Name %NewHost% -BindingInformation '*:80:%OldHost%' -PropertyName HostHeader -Value %NewHost%}"&
 powershell -Command "& {Import-Module WebAdministration; Set-WebBinding -Name %NewHost% -BindingInformation '*:443:%OldHost%' -PropertyName HostHeader -Value %NewHost%}"&
 
 
-set OldHost=lce-qa.cl.dbnetcorp.com
-set NewHost=lce-desa.cl.dbnetcorp.com
+set OldHost=lce-qa.cl.xxxxxxx.com
+set NewHost=lce-desa.cl.xxxxxxx.com
 powershell -Command "& {Import-Module WebAdministration; Rename-Item "IIS:\Sites\%OldHost%" %NewHost%}"&
 powershell -Command "& {Import-Module WebAdministration; Set-WebBinding -Name %NewHost% -BindingInformation '*:80:%OldHost%' -PropertyName HostHeader -Value %NewHost%}"&
 powershell -Command "& {Import-Module WebAdministration; Set-WebBinding -Name %NewHost% -BindingInformation '*:443:%OldHost%' -PropertyName HostHeader -Value %NewHost%}"&
--- config_ambiente/IISCambio_uat_QA.bat
+++ config_ambiente/IISCambio_uat_QA.bat
@@ -1,29 +1,29 @@
-set OldHost=fe-uat-admin.cl.dbnetcorp.com
-set NewHost=fe-qa-admin.cl.dbnetcorp.com
+set OldHost=fe-uat-admin.cl.xxxxxx.com
+set NewHost=fe-qa-admin.cl.xxxxxxx.com
 powershell -Command "& {Import-Module WebAdministration; Rename-Item "IIS:\Sites\%OldHost%" %NewHost%}"&
 powershell -Command "& {Import-Module WebAdministration; Set-WebBinding -Name %NewHost% -BindingInformation '*:80:%OldHost%' -PropertyName HostHeader -Value %NewHost%}"&
 powershell -Command "& {Import-Module WebAdministration; Set-WebBinding -Name %NewHost% -BindingInformation '*:443:%OldHost%' -PropertyName HostHeader -Value %NewHost%}"&
 
-set OldHost=cf-uat.cl.dbnetcorp.com
-set NewHost=cf-qa.cl.dbnetcorp.com
+set OldHost=cf-uat.cl.xxxxxx.com
+set NewHost=cf-qa.cl.xxxxxx.com
 powershell -Command "& {Import-Module WebAdministration; Rename-Item "IIS:\Sites\%OldHost%" %NewHost%}"&
 powershell -Command "& {Import-Module WebAdministration; Set-WebBinding -Name %NewHost% -BindingInformation '*:80:%OldHost%' -PropertyName HostHeader -Value %NewHost%}"&
 powershell -Command "& {Import-Module WebAdministration; Set-WebBinding -Name %NewHost% -BindingInformation '*:443:%OldHost%' -PropertyName HostHeader -Value %NewHost%}"&
 
-set OldHost=fe-uat.cl.dbnetcorp.com
-set NewHost=fe-qa.cl.dbnetcorp.com
+set OldHost=fe-uat.cl.xxxxxxx.com
+set NewHost=fe-qa.cl.xxxxxxx.com
 powershell -Command "& {Import-Module WebAdministration; Rename-Item "IIS:\Sites\%OldHost%" %NewHost%}"&
 powershell -Command "& {Import-Module WebAdministration; Set-WebBinding -Name %NewHost% -BindingInformation '*:443:%OldHost%' -PropertyName HostHeader -Value %NewHost%}"&
 
-set OldHost=fe-uatwss.cl.dbnetcorp.com
-set NewHost=fe-qa-wss.cl.dbnetcorp.com
+set OldHost=fe-uatwss.cl.xxxxxx.com
+set NewHost=fe-qa-wss.cl.xxxxxx.com
 powershell -Command "& {Import-Module WebAdministration; Rename-Item "IIS:\Sites\%OldHost%" %NewHost%}"&
 powershell -Command "& {Import-Module WebAdministration; Set-WebBinding -Name %NewHost% -BindingInformation '*:80:%OldHost%' -PropertyName HostHeader -Value %NewHost%}"&
 powershell -Command "& {Import-Module WebAdministration; Set-WebBinding -Name %NewHost% -BindingInformation '*:443:%OldHost%' -PropertyName HostHeader -Value %NewHost%}"&
 
 
-set OldHost=lce-uat.cl.dbnetcorp.com
-set NewHost=lce-qa.cl.dbnetcorp.com
+set OldHost=lce-uat.cl.xxxxxxx.com
+set NewHost=lce-qa.cl.xxxxxx.com
 powershell -Command "& {Import-Module WebAdministration; Rename-Item "IIS:\Sites\%OldHost%" %NewHost%}"&
 powershell -Command "& {Import-Module WebAdministration; Set-WebBinding -Name %NewHost% -BindingInformation '*:80:%OldHost%' -PropertyName HostHeader -Value %NewHost%}"&
 powershell -Command "& {Import-Module WebAdministration; Set-WebBinding -Name %NewHost% -BindingInformation '*:443:%OldHost%' -PropertyName HostHeader -Value %NewHost%}"&
